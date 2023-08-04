5e3e3bd725e516315a00aafc0d36b6ae868eb5bd
Jun Kato <i@junkato.jp>
2023-08-05 02:36:33 +0900

    Use fomantic-ui

--- src/main.tsx
+++ src/main.tsx
@@ -4,7 +4,7 @@ import ReactDOM from "react-dom/client";
 
 import App from "./App.tsx";
 
-import "semantic-ui-css/semantic.min.css";
+import "fomantic-ui-css/semantic.min.css";
 import "./main.css";
 import { BasePathContext } from "./BasePathContext.ts";
 
